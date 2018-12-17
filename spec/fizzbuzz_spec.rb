6fbf2c42c9e97e9098eebc3d3146d9f3a94d8f6b
Sebastian Lavender <sebastianlavender@outlook.com>
2018-12-17 14:49:20 +0000

    Adds tests for integer output if input not divisible by 3 or 5 and script to pass that test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,17 +10,22 @@ describe 'fizzbuzz' do
 
   it 'returns "buzz" when number is divisible by 5 but not by 3' do
     range = (1...100).to_a.reject{ |i| i % 3 == 0 }
-    number = range[rand(0...67)] * 5
+    number = range.sample * 5
     result = fizzbuzz(number)
     expect(fizzbuzz(number)).to eq 'buzz'
   end
 
   it 'returns "fizz" when number divisible by 3 but not by 5' do
     range = (1..100).to_a.reject{ |i| i % 5 == 0 }
-    number = range[rand(0...80)] * 3 #Given
+    number = range.sample * 3 #Given
     result = fizzbuzz(number) #When
     expect(fizzbuzz(number)).to eq 'fizz' #Then
   end
 
+  it 'returns number if not divisible by 3 or 5' do
+    number = (1..100).to_a.reject{ |i| i % 3 == 0 || i % 5 == 0 }.sample
+    result = fizzbuzz(number)
+    expect(fizzbuzz(number)).to eq number
+  end
 
 end
