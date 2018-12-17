7c19a57e5170992fcea1374ac5c5ce312e83812a
Sebastian Lavender <sebastianlavender@outlook.com>
2018-12-17 14:35:03 +0000

    Add test for 'buzz' and script to solve this case

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,8 +8,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(number)).to eq 'fizzbuzz'
   end
 
-  it 'returns "fizz" when number divisible by 3' do
-    range = (1...100).to_a.reject{ |i| i % 5 == 0 }
+  it 'returns "buzz" when number is divisible by 5 but not by 3' do
+    range = (1...100).to_a.reject{ |i| i % 3 == 0 }
+    number = range[rand(0...67)] * 5
+    result = fizzbuzz(number)
+    expect(fizzbuzz(number)).to eq 'buzz'
+  end
+
+  it 'returns "fizz" when number divisible by 3 but not by 5' do
+    range = (1..100).to_a.reject{ |i| i % 5 == 0 }
     number = range[rand(0...80)] * 3 #Given
     result = fizzbuzz(number) #When
     expect(fizzbuzz(number)).to eq 'fizz' #Then
